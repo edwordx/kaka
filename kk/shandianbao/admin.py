4c412c071dd9be94e61fccb220ae6761d3000d1d
imouren <mouren11@163.com>
2018-10-26 16:51:01 +0800

    x

--- kk/shandianbao/admin.py
+++ kk/shandianbao/admin.py
@@ -20,7 +20,7 @@ class SDBTokenAdmin(admin.ModelAdmin):
 
 @admin.register(models.SDBTrade)
 class SDBTradeAdmin(admin.ModelAdmin):
-    list_display = ["id", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "update_time"]
+    list_display = ["trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "update_time"]
     fields = ["trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type"]
     search_fields = ["trans_id", "terminal", "trade_date", "merchant"]
     list_filter = ["card_type", "trade_type", "trade_status", "return_code"]
@@ -28,7 +28,7 @@ class SDBTradeAdmin(admin.ModelAdmin):
 
 @admin.register(models.SDBTerminal)
 class SDBTerminalAdmin(admin.ModelAdmin):
-    list_display = ["id", "terminal", "batch", "company", "pos_type", "pos_version", "agent", "agent_name", "bind_status", "activate_status", "bind_merchant", "bind_time", "update_time"]
+    list_display = ["terminal", "batch", "company", "pos_type", "pos_version", "agent", "agent_name", "bind_status", "activate_status", "bind_merchant", "bind_time", "update_time"]
     fields = ["terminal", "batch", "company", "pos_type", "pos_version", "agent", "agent_name", "bind_status", "activate_status", "bind_merchant", "bind_time"]
     search_fields = ["terminal", "bind_status", "activate_status", "bind_time", "agent"]
 
@@ -36,17 +36,37 @@ class SDBTerminalAdmin(admin.ModelAdmin):
 @admin.register(models.SDBPos)
 class SDBPosAdmin(admin.ModelAdmin):
     form = fms.AdminSDBPosForm
-    list_display = ["id", "user", "terminal", "create_time", "update_time"]
+    list_display = ["user", "nickname", "terminal", "create_time", "update_time"]
     fields = ["user", "terminal"]
     search_fields = ["terminal", "user__username"]
+
+    def nickname(self, obj):
+        user = obj.user
+        if hasattr(user, "userprofile"):
+            name = user.userprofile.name
+        else:
+            name = u"无"
+        return name
+    nickname.allow_tags = True
+    nickname.short_description = u'昵称'
 
 
 @admin.register(models.SDBFenRun)
 class SDBFenRunAdmin(admin.ModelAdmin):
     form = select2_modelform(models.SDBFenRun)
-    list_display = ["id", "user", "hardware_point", "point", "hardware_point_yun", "point_yun", "hardware_point_yin", "point_yin", "hardware_point_wx", "point_wx", "profit", "tax", "create_time", "update_time"]
+    list_display = ["user", "nickname", "hardware_point", "point", "hardware_point_yun", "point_yun", "hardware_point_yin", "point_yin", "hardware_point_wx", "point_wx", "profit", "tax", "create_time", "update_time"]
     fields = ["user", "hardware_point", "point", "hardware_point_yun", "point_yun", "hardware_point_yin", "point_yin", "hardware_point_wx", "point_wx", "profit", "tax"]
     search_fields = ["user__username"]
+
+    def nickname(self, obj):
+        user = obj.user
+        if hasattr(user, "userprofile"):
+            name = user.userprofile.name
+        else:
+            name = u"无"
+        return name
+    nickname.allow_tags = True
+    nickname.short_description = u'昵称'
 
 
 @admin.register(models.SDBUserRMB)
@@ -90,35 +110,75 @@ class SDBUserRMBAdmin(admin.ModelAdmin):
 
 @admin.register(models.SDBProfit)
 class SDBProfitAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "rmb", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
+    list_display = ["user", "nickname", "rmb", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     fields = ["user", "rmb", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     readonly_fields = fields
     search_fields = ["user__username", "trans_id", "terminal", "trade_date", "merchant"]
     list_filter = ["point_type"]
 
+    def nickname(self, obj):
+        user = obj.user
+        if hasattr(user, "userprofile"):
+            name = user.userprofile.name
+        else:
+            name = u"无"
+        return name
+    nickname.allow_tags = True
+    nickname.short_description = u'昵称'
+
 
 @admin.register(models.SDBChildOneProfit)
 class SDBChildOneProfitAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
+    list_display = ["user", "nickname", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     fields = ["user", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     readonly_fields = fields
     search_fields = ["user__username", "trans_id", "terminal", "trade_date", "merchant"]
     list_filter = ["point_type"]
+
+    def nickname(self, obj):
+        user = obj.user
+        if hasattr(user, "userprofile"):
+            name = user.userprofile.name
+        else:
+            name = u"无"
+        return name
+    nickname.allow_tags = True
+    nickname.short_description = u'昵称'
 
 
 @admin.register(models.SDBChildTwoProfit)
 class SDBChildTwoProfitAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
+    list_display = ["user", "nickname", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     fields = ["user", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     readonly_fields = fields
     search_fields = ["user__username", "trans_id", "terminal", "trade_date", "merchant"]
     list_filter = ["point_type"]
+
+    def nickname(self, obj):
+        user = obj.user
+        if hasattr(user, "userprofile"):
+            name = user.userprofile.name
+        else:
+            name = u"无"
+        return name
+    nickname.allow_tags = True
+    nickname.short_description = u'昵称'
 
 
 @admin.register(models.SDBChildThreeProfit)
 class SDBChildThreeProfitAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
+    list_display = ["user", "nickname", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     fields = ["user", "rmb", "diff_point", "point_type", "point", "hardware_point", "profit", "tax", "trans_id", "merchant", "trade_date", "trade_rmb", "trade_type", "trade_status", "card_code", "card_type", "return_code", "return_desc", "terminal", "agent_level", "agent", "business_type", "status", "create_time", "pay_time"]
     readonly_fields = fields
     search_fields = ["user__username", "trans_id", "terminal", "trade_date", "merchant"]
     list_filter = ["point_type"]
+
+    def nickname(self, obj):
+        user = obj.user
+        if hasattr(user, "userprofile"):
+            name = user.userprofile.name
+        else:
+            name = u"无"
+        return name
+    nickname.allow_tags = True
+    nickname.short_description = u'昵称'
